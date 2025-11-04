a968c3d1f2b95c3b5ea92e77b5f19da22ea66b4f
Jofre <jofre.cuerda@gmail.com>
2025-11-04 15:33:14 +0100

    cambio pueto gmail

--- src/main/java/org/example/service/KillerKissService.java
+++ src/main/java/org/example/service/KillerKissService.java
@@ -215,9 +215,9 @@ public class KillerKissService {
     private boolean enviarCorreu(String destinatari, String missatge, String assumpte) {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.smtp.ssl.protocols", "TLSv1.2");
 
         Session session = Session.getInstance(props, new Authenticator() {
